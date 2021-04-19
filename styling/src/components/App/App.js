170a5cd6aa39aa23dca8b5dbff0e68454f27661d
Nedim Begić <nedim@renesalis-packaging.com>
2021-04-19 13:44:38 +0200

    App.js

--- styling/src/components/App/App.js
+++ styling/src/components/App/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Alert from '../Alert/Alert';
+
 function App() {
-  return <></>;
+  return (
+    <div className="wrapper">
+      <Alert />
+    </div>
+  )
 }
 
 export default App;
